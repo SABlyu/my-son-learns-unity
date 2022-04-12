cc3544cad2da0a4a2371161fd7ec6a685833c2db
Brian Jones <sablyu@live.com>
2022-04-12 18:53:08 +0200

    Camera rotation

--- New Unity Project/Assets/MouseLook.cs
+++ New Unity Project/Assets/MouseLook.cs
@@ -9,11 +9,14 @@ public class MouseLook : MonoBehaviour
 
     public Transform playerBody;
 
+    float xRotation = 0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -21,6 +24,11 @@ public class MouseLook : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
         playerBody.Rotate(Vector3.up * mouseX);
     }
