8dec1633f04c101b34adaf0fe82bf3250e5fba8b
Brian Jones <sablyu@live.com>
2022-05-11 19:19:41 +0200

    go_run_fall_go up stairs

--- New Unity Project/Assets/playerMovement.cs
+++ New Unity Project/Assets/playerMovement.cs
@@ -7,6 +7,14 @@ public class playerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 20f;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +25,13 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float Z = Input.GetAxis("Vertical");
 
@@ -24,5 +39,11 @@ public class playerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * Z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime; // velocity.y = velocity.y + grav...
+
+        controller.Move(velocity * Time.deltaTime);
+
+
     }
 }
