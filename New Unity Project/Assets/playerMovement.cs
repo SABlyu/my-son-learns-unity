99c20f03d2bf882b8280ff49bd697fefcb97f6e0
Brian Jones <sablyu@live.com>
2022-05-11 17:52:40 +0200

    control

--- New Unity Project/Assets/playerMovement.cs
+++ New Unity Project/Assets/playerMovement.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovment : MonoBehaviour
+public class playerMovement : MonoBehaviour
 {
+    public CharacterController controller;
+
+    public float speed = 20f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float Z = Input.GetAxis("Vertical");
+
+
+        Vector3 move = transform.right * x + transform.forward * Z;
+
+        controller.Move(move * speed * Time.deltaTime);
     }
 }
